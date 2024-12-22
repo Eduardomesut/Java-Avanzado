b3b9d2b07b737ba9019ea78c86720e3e8aeaa751
eduardomesut <eduardomesut@gmail.com>
2024-12-22 17:36:49 +0100

    Nuevo

---
+++ src/main/java/funciones/Pruebas.java
@@ -0,0 +1,19 @@
+package funciones;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Pruebas {
+
+    public static void main(String[] args) {
+        ArrayList<String> nombres = new ArrayList<>();
+
+        nombres.add("Jaime");
+        nombres.add("Pepe");
+        nombres.add("Matt");
+
+        //nombres.stream().forEach(x -> System.out.println(x.toUpperCase()));
+        Stream<String> resultado = nombres.stream().map(x -> x.toUpperCase());
+        resultado.forEach(x -> System.out.println(x));
+    }
+}
