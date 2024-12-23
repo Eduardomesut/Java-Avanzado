2795545ec952258f5a08fa348c84b50689b7f182
eduardomesut <eduardomesut@gmail.com>
2024-12-23 12:55:36 +0100

    Seguir avanzado

--- src/main/java/funciones/Pruebas.java
+++ src/main/java/funciones/Pruebas.java
@@ -1,6 +1,8 @@
 package funciones;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Pruebas {
@@ -14,7 +16,16 @@ public class Pruebas {
         nombres.add("Jonny");
 
         //nombres.stream().forEach(x -> System.out.println(x.toUpperCase()));
-        Stream<String> resultado = nombres.stream().map(x -> x.toUpperCase());
+        Stream<String> resultado = nombres.stream()
+                .map(x -> x.toUpperCase())
+                .filter(x -> x.startsWith("P"));
+
         resultado.forEach(x -> System.out.println(x));
+
+        int[]numeros = {1,2,3,4,5,6,7,8,9}; 
+        IntStream numsStream = Arrays.stream(numeros);
+        numsStream.filter(x -> x%2 == 0)
+                .forEach(x -> System.out.println(x));
+
     }
 }
